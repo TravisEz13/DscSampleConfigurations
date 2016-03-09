b026daf828605f63fe7b60b656645c1397ecd780
Travis Plunk <travis@ez13.net>
2016-03-09 14:22:22 -0800

    Added sample to install dotnet cli

---
+++ DotNetCli.ps1
@@ -0,0 +1,14 @@
+﻿configuration dotnet
+{
+  Import-dscresource -ModuleName xpsdesiredstateconfiguration
+  xPackage dotnet
+  {
+    Name = 'Microsoft Dotnet CLI for Windows (1.0.0.001598 )'
+    Path = 'https://dotnetcli.blob.core.windows.net/dotnet/beta/Installers/1.0.0.001598/dotnet-win-x64.1.0.0.001598.exe'
+    ProductId = '{7DE53F51-B432-412B-9447-943A5138457A}'
+    Arguments =  '/install /passive /norestart'
+    
+  }
+}
+dotnet
+Start-DscConfiguration -path .\dotnet -Wait -Verbose -force
