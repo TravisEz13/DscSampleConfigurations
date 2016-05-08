b759151b9cfc25c7db35ca4886f9063fc2b88f3e
Travis Plunk <travis.plunk@microsoft.com>
2016-05-08 16:34:06 -0700

    make sure psgallery is trusted

--- DevWorkstation/prereqs.ps1
+++ DevWorkstation/prereqs.ps1
@@ -1,3 +1,4 @@
 Install-Module -repository psgallery  cChoco
 Install-Module -repository psgallery  xpsdesiredstateconfiguration
 Install-Module -repository psgallery  PackageManagementProviderResource
+Set-PSRepository -Name psgallery -InstallationPolicy Trusted
